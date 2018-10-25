a7752573eeda5255c5b09bc7c2b54afd77b31ffd
Xolani-Mxoxozi <37544169+Xolani-Mxoxozi@users.noreply.github.com>
2018-10-25 16:33:02 +0200

    Update ex16.py

--- exercises/ex16.py
+++ exercises/ex16.py
@@ -29,8 +29,8 @@ target.write(line1)
 target.write("\n")
 target.write(line2)
 target.write("\n")
-target.write(line3)
-target.write("\n")
+# target.write(line3)
+# target.write("\n")
 
 print "And finally, we close it."
 target.close()
